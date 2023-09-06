775b27d3b1e54f4fd58af5e56d846bf74c65014e
PlyTruth <qianren1024@gmail.com>
2023-09-06 19:55:55 +0100

    refactor: delete dice and rename vdf

--- examples/vdf-service/migrations/2_deploy_vdf_core.js
+++
@@ -1,7 +0,0 @@
-var VdfService = artifacts.require("./VdfService.sol");
-var VdfResolver = artifacts.require("./VdfResolver.sol");
-
-module.exports = function(deployer) {
-  deployer.deploy(VdfService);
-  deployer.deploy(VdfResolver);
-};
--- examples/vdf-service/migrations/3_deploy_vdf_interface.js
+++
@@ -1,5 +0,0 @@
-var UsingVdfService = artifacts.require("./UsingVdfService.sol");
-
-module.exports = function(deployer) {
-  deployer.deploy(UsingVdfService);
-};
