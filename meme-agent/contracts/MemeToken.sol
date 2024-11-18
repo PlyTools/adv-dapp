1b5464cebcdc933a7815ee34e8b7c879708ad650
PlyTruth <qianren1024@gmail.com>
2024-11-18 17:35:41 +0000

    feat: add meme-agent to testing AI agent and meme

---
+++ meme-agent/contracts/MemeToken.sol
@@ -0,0 +1,11 @@
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract MemeToken is ERC20 {
+    constructor(string memory name, string memory symbol) ERC20(name, symbol) {
+        _mint(msg.sender, 1000000 * 10 ** decimals());
+    }
+}
